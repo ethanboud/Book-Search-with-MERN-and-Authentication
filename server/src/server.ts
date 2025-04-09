6e5e260abe32b3e96f79f77aecc50b25c5663016
ethanboud <ethanboud@gmail.com>
2025-04-08 23:38:19 -0600

    correction to the drname and filename variables in the server file

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,11 @@ import path from 'node:path';
 import db from './config/connection.js';
 import { authenticateToken } from './utils/auth.js';
 import type { Request, Response } from 'express';
+import { fileURLToPath } from 'node:url';
 
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,
